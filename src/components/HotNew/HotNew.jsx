5d78fe17b52d4d89a0141aa5e87f1320284431d8
Miguel <mirkouda@gmail.com>
2022-11-25 19:16:39 +0100

    bug fixed

--- src/components/HotNew/HotNew.jsx
+++ src/components/HotNew/HotNew.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../../context/GlobalState'
 
 const HotNew = (props) => {
   const {hotNews} = useContext(GlobalContext)
