3573af7117c7ad672677f74e8ab60c29ef013335
Miguel <mirkouda@gmail.com>
2022-11-25 14:02:57 +0100

    API fixed

--- src/components/ListNews/ListNews.jsx
+++ src/components/ListNews/ListNews.jsx
@@ -1,29 +1,39 @@
-// import React, { useEffect } from 'react'
-// import './ListNews.scss'
-// import axios from "axios"
+import React, { useEffect } from 'react'
+import './ListNews.scss'
+import axios from "axios"
+import { useState } from 'react'
 
-// const ListNews = () => {
-//     const [news]
-//     const getNews = async () => {
-//         try {
-//             const res = await axios.get("https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=yourkey")
-//             console.log("respons", res)
-//         } catch (error) {
-//             console.error(error)
-//         }
-//     }
+const ListNews = () => {
+    const [hotNews, setHotNews] = useState([])
+    const getNews = async () => {
+        try {
+            const res = await axios.get("https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=6waGlV96leHne0loR2SKQPUH1fGrjAqk")
+            setHotNews(res.data.results)
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
-//     useEffect(() => {
-//         getNews()
-//     }, [])
+    useEffect(() => {
+        getNews()
+    }, [])
 
+    
+    return (
+        <>
+            <div>These are the latest world news.
+                {hotNews.map((hotNew) => {
 
-//     return (
-//         <>
-//             <div>These are the latest world news.
-//             </div>
-//         </>
-//     )
-// }
+                   return(
+                    <div>
+                   <p>{hotNew.book_title}</p>
+                   <p>{hotNew.summary}</p>
+                   </div>
+                   )
+                })}
+            </div>
+        </>
+    )
+}
 
-// export default ListNews
+export default ListNews
